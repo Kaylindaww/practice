2ef67c142a58f1f0321c9ef1af4e3b4f9a0e6fae
Iminzah <sharroniminzah@gmail.com>
2021-03-02 10:51:29 +0300

    practice

---
+++ src/my work.kt
@@ -0,0 +1,26 @@
+fun main(){name()
+          modulus()
+    var kay =sum(10,20,5,8)
+     println(kay)
+    aboutme()
+
+}
+fun name(){
+    var name="hello kay"
+    println(name)
+}
+fun modulus(){
+    var a = 30
+    var b = 5
+    var c = a % b
+    println(c)
+}
+fun sum(num1:Int,num2:Int,num3:Int,num4:Int){
+    var result =num1+num2+num3+num4
+    return
+}
+fun aboutme(){
+  var aboutme ="i love myself so dearly"
+  println(aboutme)
+}
+
